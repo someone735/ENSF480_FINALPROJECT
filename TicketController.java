1616a3305080425cd87a73c8eff94ad1f985832a
julienrabino <julienmescha.rabino@ucalgary.ca>
2024-11-22 09:45:54 -0700

    Update TicketController.java
    skeleton code for TicketController

--- TicketController.java
+++ TicketController.java
@@ -1,7 +1,31 @@
 public class TicketController {
-    public Ticket purchaseTicket(Showtime showtime, User user, int Seat){
-        Ticket ticket = new Ticket();
+    TicketController(){
+    }
+    public Ticket purchaseTicket(Showtime showtime, User user, int seat){
+        int ticketID = produceTicketID();
+        double ticketPrice = 13.50; // is a default price ticket ok ?
 
-        return 
+        // query DB if seat is still available
+        // if not, print "Seat unavailable" and return null
+
+        Ticket ticket = new Ticket( ticketID, showtime, user, seat,  ticketPrice, user.getPaymentMethod());
+        BillingSystem billingSystem = new BillingSystem();
+        billingSystem.processTicketPayment(user, ticket);
+        
+        return ticket;
+    }
+
+    public String cancelTicket(Ticket ticket, User user){
+
+    }
+
+    public void sendTicketReciept(Ticket ticket, User user){
+
+    }
+
+    public int produceTicketID(){
+        // queries DB for next available ticketID
+        return 1; // just a temp  val
+
     }
 }
