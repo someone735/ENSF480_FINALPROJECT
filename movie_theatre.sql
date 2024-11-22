f67a93de0db687b5e4dc925b5232e16ffda83681
millainel <144377899+millainel@users.noreply.github.com.>
2024-11-22 01:12:02 -0700

    started on sql file
    still needs some work tho

---
+++ movie_theatre.sql
@@ -0,0 +1,100 @@
+DROP DATABASE IF EXISTS MOVIE_THEATRE;
+CREATE DATABASE MOVIE_THEATRE; 
+USE MOVIE_THEATRE;
+
+DROP TABLE IF EXISTS THEATRE;
+CREATE TABLE THEATRE (
+	TheatreID				SERIAL PRIMARY KEY,
+	Address					varchar(100) not null,
+	TheatreName				varchar(100) not null
+);
+
+DROP TABLE IF EXISTS MOVIE;
+CREATE TABLE MOVIE (
+	MovieID					SERIAL PRIMARY KEY,
+	Title					varchar(50)	not null,
+	Genre					varchar(30)	not null,
+    Theatre					int not null,
+    foreign key (Theatre) references THEATRE(TheatreID)
+);
+
+DROP TABLE IF EXISTS LOCATION;
+CREATE TABLE LOCATION (
+	LocationID				SERIAL PRIMARY KEY,
+	Address					varchar(100)	not null,
+	LocationName			varchar(100)	not null
+);
+
+
+
+DROP TABLE IF EXISTS SHOWTIME;
+CREATE TABLE SHOWTIME (
+	ShowtimeID				SERIAL PRIMARY KEY,
+	Movie					int	not null,
+	Time					varchar(10)	not null,
+    Theatre					int not null,
+    foreign key (Movie) references MOVIE(MovieID),
+    foreign key (Theatre) references THEATRE(TheatreID)
+);
+
+DROP TABLE IF EXISTS REG_USER;
+CREATE TABLE REG_USER (
+	ID					SERIAL PRIMARY KEY,
+	Fname				varchar(50)	not null,
+	Lname				varchar(50)	not null,
+    Email				varchar(50)	not null,
+    Address				varchar(50)	not null,
+    MemberShipJoinDate	DATE NOT NULL,			
+    MemberShipExpiry	DATE NOT NULL,
+    PaymentMethod		varchar(50),
+    
+	primary key (ID)
+);
+
+DROP TABLE IF EXISTS SEAT;
+CREATE TABLE SEAT (
+	SeatID				SERIAL PRIMARY KEY,
+    Showtime			int not null,
+    Theatre				int not null,
+    Movie				int not null,
+	SeatRow				char(1)	not null,
+	SeatColumn			int	not null,
+    Available			int not null,
+    RegUser				int,
+
+	primary key (seatID),
+    foreign key (RegUser) references REG_USER(ID),
+    foreign key (Theatre) references THEATRE(TheatreID),
+    foreign key (Movie) references MOVIE(MovieID),
+    foreign key (Showtime) references SHOWTIME(ShowtimeID)
+);
+
+INSERT INTO THEATRE (Address, TheatreName)
+VALUES
+('Location A','AcmePlex: Location A'),
+('Location B','AcmePlex: Location A'),
+('Westhills','AcmePlex: Westills'),
+('Candy Cane Lane, North Pole','AcmePlex: Santa Edition');
+
+INSERT INTO MOVIE (Title, Genre, Theatre)
+VALUES
+('The Garfeild Movie','Fat Cat, Animated, Lasagna', 2),
+('The Wild Robot','Adventure, Family, Animated', 2),
+('The Barbie Movie','Societal Issues', 1),
+('Barbie as the Three Musketeers','Action, Friendship, Animated', 1),
+('Barbie as the Island Princess','Tropical, Adventure, Animated', 1),
+('Barbie in the Princess Charm School','Magical, Redemption Arc, Animated', 1),
+('The Garfeild Movie','Fat Cat, Animated, Lasagna', 1),
+('The Spongebob Movie','Comedy, Adventure, Animated', 3),
+('Clueless','Rom-Com', 3),
+('Diary of a Wimpy Kid: Rodrick Rules','Comedy, Slice of Life?', 3),
+('Kimi No Nawa','Anime, Romance', 3),
+('The Garfeild Movie','Fat Cat, Animated, Lasagna', 3),
+('Home Alone','Christmas, Comedy', 4),
+('Home Alone 2','Christmas, Comedy', 4),
+('Home Alone 3','Christmas, Comedy', 4),
+('The Garfeild Movie','Fat Cat, Animated, Lasagna', 4),
+('The Grinch','Christmas, Kindness, Animated', 4);
+
+
+
