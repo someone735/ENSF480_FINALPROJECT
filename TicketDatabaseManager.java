915f3f6ba0721b2da512e32fa37683cf43bee426
julienrabino <julienmescha.rabino@ucalgary.ca>
2024-11-24 09:14:57 -0700

    made new files for db managers

---
+++ TicketDatabaseManager.java
@@ -0,0 +1,28 @@
+public class TicketDatabaseManager {
+    TicketDatabaseManager(){}
+
+    public boolean addTicketToDB(Ticket ticket) {
+        return false;
+    }
+
+    public boolean updateTicketStatus(Ticket ticket) {
+        return false;
+    }
+
+    public int produceTicketID() {
+        return 0;
+    }
+
+    public boolean fetchCancellationEligibility(Ticket ticket) {
+        return false;
+    }
+
+    public Ticket fetchTicket(int ticketID) {
+        return null;
+    }
+
+    public boolean updateShowtimeSeats(Showtime showtime) {
+        return false;
+    }
+
+}
---
+++ UserDatabaseManager.java
@@ -0,0 +1,11 @@
+public class UserDatabaseManager {
+
+    UserDatabaseManager(){}
+
+    public RegisteredUser fetchUser(int id){
+        // TO DO:
+        // query db to find user's details
+        // must use db info to make an RU Object, which should be returned
+        return null;
+    }
+}
